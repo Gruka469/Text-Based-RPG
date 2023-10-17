f27aa0485c7d58c3fdf07bf4f5f9d98ca8c2f48d
Gruka469 <lifetroll6@gmail.com>
2023-10-17 14:51:43 +0300

    readability update

--- characters.py
+++ characters.py
@@ -1,86 +1,67 @@
-start = input("Do you wanna play the game? y/n? ")
-players = input("How many players are going to play the game? 1-4: ")
-character = input("Each player must choose their character: ")
+# This is the first file in which I'm going to define all the characters and inputs that the players
+# have to make to play the game.
 
-class characters:
-    def __init__(self, name, health, attack, defense, agility):
+# I am also storing the players initial information i.e. their names
+
+class Character:
+    def __init__(self, name, health, attack, attack_speed, defense):
         self.name = name
         self.health = health
         self.attack = attack
+        self.attack_speed = attack_speed
         self.defense = defense
-        self.agility = agility
-    
-    def take_damage(self, damage):
-        self.health -= damage
 
-class Mage(character):
-    def __init__(self, name):
-        super().__init__(name, health = 450, attack = 20, attack_speed = 0.80, defense = 30)
-        self.mana = 350
+class Player:
+    def __init__(self, name, character):
+        self.name = name
+        self.character = character
 
-class Warrior(character):
-    def __init__(self, name):
-        super().__init__(name, health = 610, attack = 15, attack_speed = 1.00, defense = 80)
-        self.rage = 100
+    def display_info(self):
+        return f"{self.name}: {self.character.name} (Health: {self.character.health},\
+              Attack: {self.character.attack}, Defense: {self.character.defense})"
 
-class Rogue(character):
-    def __init__(self, name):
-        super().__init__(name, health = 520, attack = 8, attack_speed = 1.18, defense = 60)
-        self.energy = 100
+# Create a dictionary to map character names to Character instances
+character_dict = {
+    'Warrior': Character('Warrior', 610, 15, 1.00, 80),
+    'Rogue': Character('Rogue', 520, 8, 1.18, 60),
+    'Mage': Character('Mage', 450, 20, 0.80, 30),
+    'Druid': Character('Druid', 480, 12, 1.03, 50),
+    'Priest': Character('Priest', 400, 20, 0.80, 30),
+    'Paladin': Character('Paladin', 650, 15, 0.90, 90),
+    'Hunter': Character('Hunter', 500, 10, 1.10, 50),
+    'Warlock': Character('Warlock', 450, 25, 0.75, 30),
+    'Shaman': Character('Shaman', 550, 15, 1.00, 75)
+}
 
-class Druid(character):
-    def __init__(self, name):
-        super().__init__(name, health = 480, attack = 12, attack_speed = 1.03, defense = 50)
-        self.mana = 350
-        self.rage = 100
-        self.energy = 100
+start = input("Do you wanna play the game? y/n? ")
+players = input("How many players are going to play the game? ")
+list_players = players.split()
+character = input("Each player must choose their character: ")
 
-class Preist(character):
-    def __init__(self, name):
-        super().__init__(name, health = 400, attack = 20, attack_speed = 0.80, defense = 30)
-        self.mana = 380
 
-class Paladin(character):
-    def __init__(self, name):
-        super().__init__(name, health = 650, attack = 15, attack_speed = 0.90, defense = 90)
-        self.mana = 330
 
-class Hunter(character):
-    def __init__(self, name):
-        super().__init__(name, health = 500, attack = 10, attack_speed = 1.10, defense = 50)
-        self.mana = 320
 
-class Warlock(character):
-    def __init__(self, name):
-        super().__init__(name, health = 450, attack = 25, attack_speed = 0.75, defense = 30)
-        self.mana = 380
-
-class Shaman(character):
-    def __init__(self, name):
-        super().__init__(name, health = 550, attack = 15, attack_speed = 1.00, defense = 75)
-        self.mana = 340
-
-while character > 4 or character == "Done":
-    character = input("Next character: ")
+while players <= 9:
+    character = input(f"{players}: Choose your character (Warrior, Rogue, Mage, etc.): ")
 
     if character == 'Warrior':
-        pass
+        print(f"{players}: You've selected {character} as your character.")
     elif character == 'Rogue':
-        pass
+        print(f"{players}: You've selected {character} as your character.")
     elif character == 'Mage':
-        pass
+        print(f"{players}: You've selected {character} as your character.")
     elif character == 'Druid':
-        pass
+        print(f"{players}: You've selected {character} as your character.")
     elif character == 'Preist':
-        pass
+        print(f"{players}: You've selected {character} as your character.")
     elif character == 'Paladin':
-        pass
+        print(f"{players}: You've selected {character} as your character.")
     elif character == 'Hunter':
-        pass
+        print(f"{players}: You've selected {character} as your character.")
     elif character == 'Warlock':
-        pass
+        print(f"{players}: You've selected {character} as your character.")
     elif character == 'Shaman':
-        pass
+        print(f"{players}: You've selected {character} as your character.")
     else:
         print("Please choose a valid character: ")
         continue
